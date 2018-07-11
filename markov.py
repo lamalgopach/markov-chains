c4827840a48c901dcd3f3698ca95a03876758267
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-10 18:03:39 -0700

    Automating function make chains

--- markov.py
+++ markov.py
@@ -61,18 +61,73 @@ def make_chains(text_string):
 
     return chains
 
+def make_chains_n(text_string):
+    """Take input text as string; return dictionary of Markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two 
+    words in the input text.
+
+    For example:
+
+        >>> chains = make_chains("hi there mary hi there juanita")
+
+    Each bigram (except the last) will be a key in chains:
+
+        >>> sorted(chains.keys())
+        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+    Each item in chains is a list of all possible following words:
+
+        >>> chains[('hi', 'there')]
+        ['mary', 'juanita']
+        
+        >>> chains[('there','juanita')]
+        [None]
+    """
+    chains = {}
+    text_string = text_string.split()
+
+
+    for word in range(len(text_string)-2):
+    	#making tuples out of each  pair of consecutive words in the string
+    	#word_key = (text_string[word],text_string[word+1])
+    	word_key = ()
+
+    	# make tuple out of 2 consecutive words using loop
+    	# need to make it so that each time it takes 2 DIFFERENT indexes
+
+    	for index in range(2):
+    	    word_key = word_key + (text_string[index],)
+    	print (word_key)
+
+
+
+    	#values in the dictionary will be lists of possible consecutive words
+    	if word_key in chains:
+    		chains[word_key] += [text_string[word+2]]
+    	else:
+    		chains[word_key] = [text_string[word+2]]
+
+    return chains
+
+
+
+
+
+
 
 def make_text(chains):
     """Return text from chains.
     The function gets the dictionary "chains" as an input, 
     and returns a text as a string of randomized words as output. 
-    The text is built in a way that every 2 consecutive words fit together. 
+    The text is built in a way that every 2 consecutive words fit together.
+    (bigrams) 
 		
     """
 
     words = []
 
-    next_key = ""
     key = choice(list(chains.keys()))
     word1 = key[0]
     word2 = key[1]
@@ -92,6 +147,42 @@ def make_text(chains):
     return " ".join(words)
 
 
+# def make_text_n(chains,n):
+#     """Return text from chains.
+#     The function gets the dictionary "chains" as an input, 
+#     and returns a text as a string of randomized words as output. 
+#     The text is built in a way that every n consecutive words fit together. 
+#     (n-grams)
+		
+#     """
+
+#     words = []
+
+#     next_key = ""
+#     key = choice(list(chains.keys()))
+#     for in 
+#     	#add n words in key
+
+#     word1 = key[0]
+#     word2 = key[1]
+
+#     words.append(word1)
+#     words.append(word2)
+    
+#     while key in chains:
+#     	value = chains[key]
+#     	word3 = choice(value)
+
+#     	words.append(word3)
+#     	key = (word2, word3)
+#     	word2 = word3
+    
+
+#     return " ".join(words)
+
+
+
+#input_path = "gettysburg.txt"
 input_path = "green-eggs.txt"
 
 # Open the file and turn it into one long string
@@ -103,4 +194,6 @@ chains = make_chains(input_text)
 # Produce random text
 random_text = make_text(chains)
 
-print(random_text)
+#print(random_text)
+make_chains_n(input_text)
+
